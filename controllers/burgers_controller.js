6720343ca9e19ef9ed7cbeef94d76a175b3c9f5e
sbstiane46 <sbstian89@yahoo.com>
2019-09-17 16:01:44 -0400

    progress

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,27 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hdbrsObj = {
+        burgers: data
+      };
+      console.log(hdbrsObj);
+      res.render("index", hdbrsObj);
+    });
+  
+  
+      console.log("condition", condition);
+      burger.update({ devoured: req.body.devoured }, condition, function(
+        result
+      ) {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+    });
+
+  module.exports = router;
