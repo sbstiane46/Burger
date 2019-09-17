c3df7104672269d98094508b7acabc50f5f36402
sbstiane46 <sbstian89@yahoo.com>
2019-09-17 16:29:04 -0400

    added dotenv to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "123abc",
-    database: "burgers_db"
-});
+require("dotenv").config();
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+     connection = mysql.createConnection({
+        host:"localhost",
+        user: "root",
+        password: "123abc",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err){
     if(err)throw err;
